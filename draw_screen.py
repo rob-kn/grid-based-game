de3aba7a93bb5359e5fec52860c27d4aef4dc5fd
Rob <rob.knights@hotmail.com>
2017-11-02 00:47:49 +0000

    Enemy and Player class working. Camera class is able to draw these in relation to each other using the players offset.

--- draw_screen.py
+++ draw_screen.py
@@ -4,66 +4,104 @@ import server_sim as server
 
 #TODO : split into draw_map_camera(camx, camy) and draw_entities() (both funcs of a class)
 
+
 class Camera:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.center_to_xedge = int((self.width - 1) / 2)     # 5
+        self.center_to_yedge = int((self.height - 1) / 2)    # 6
+        self.center_x = self.width - self.center_to_xedge
+        self.center_y = self.height - self.center_to_yedge
         self.map = [line.strip() for line in open('maps/level_0_map.txt').readlines()]
+        self.images = {'#': conf.wall_img,
+                       ' ': conf.floor_img}
+        self.player = None
 
-    def draw_camera_map(self, cam_x, cam_y):
-        for y in range(self.height):
-            for x in range(self.width):
-                tile_position_x = (x * conf.TILE_WIDTH) - cam_x
-                tile_position_y = (y * conf.TILE_HEIGHT) - cam_y
-                if onscreen(tile_position_x, tile_position_y):
-                    self.map[y][x].draw(conf.screen, x * conf.TILE_WIDTH, y * conf.TILE_HEIGHT)
+    def draw_camera_map(self, grid_x, grid_y, offset):
+        conf.screen.fill((0, 0, 0))
+        range_x, range_y = self.get_onscreen_range(grid_x, grid_y, offset)
+        for col_count, y in enumerate(range_y):
+            y_pixel = (col_count-1) * conf.GRID_SQUARE_SIZE
+            for row_count, x in enumerate(range_x):
+                x_pixel = (row_count-1) * conf.GRID_SQUARE_SIZE
+                rect = pg.Rect(x_pixel + offset[0], y_pixel + offset[1], conf.GRID_SQUARE_SIZE, conf.GRID_SQUARE_SIZE)
+                conf.screen.blit(self.images[self.map[y][x]], rect)
 
-    def on_screen(self, x, y):
-        return not (x < conf.GRID_SQUARE_SIZE or x > conf.SCREEN_WIDTH or
-                    y < conf.GRID_SQUARE_SIZE or y > conf.SCREEN_HEIGHT)
 
-def draw_camera_view(player):
-    """
-    This function will draw the current camera view of the game.
-    It draws the relevant tiles only with a small overlap around the game window.
-    It shifts the map by the current offset of the player.
-    The player is always drawn in the center of the map.
-    """
-    current_grid_pos = (player.x, player.y)
-    current_moving_offset = player.offset
+    def get_onscreen_range(self, grid_x, grid_y, offset):
+        """
+        This function takes an x and y and returns the ranges of tiles that should be drawn.
+        It adds a 1 tile border to account for any player movement.
+        """
+        range_x = range(grid_x - (self.center_to_xedge + 1), grid_x + (self.center_to_xedge + 1) + 1)
+        range_y = range(grid_y - (self.center_to_yedge + 1), grid_y + (self.center_to_yedge + 1) + 1)
+        return range_x, range_y
 
-    # starting drawing points are off screen to allow for whichever direction the player is moving
-    starting_x, starting_y = current_grid_pos[0] - conf.CENTER_X, current_grid_pos[1] - conf.CENTER_Y
-    # This blanks the screen
-    conf.screen.fill((0, 0, 0))
-    # This loop draws the map around the player. It loops through with a 1 tile border.
-    for y, row in enumerate(conf.COMPLETE_GRID[starting_y: starting_y+conf.CAMERA_HEIGHT+2]):
-        for x, tile in enumerate(row[starting_x: starting_x+conf.CAMERA_WIDTH+2]):
-            if tile == '#':
-                wall_rect = pg.Rect(((x-1) * conf.GRID_SQUARE_SIZE)+current_moving_offset[0], ((y-1) * conf.GRID_SQUARE_SIZE)+current_moving_offset[1], conf.GRID_SQUARE_SIZE,
-                                      conf.GRID_SQUARE_SIZE)
-                conf.screen.blit(conf.wall_img, wall_rect)
-            elif tile == ' ':
-                floor_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
-                        ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1], conf.GRID_SQUARE_SIZE,
-                        conf.GRID_SQUARE_SIZE)
-                conf.screen.blit(conf.floor_img, floor_rect)
-            elif tile == 'T':
-                floor_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
-                                     ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1],
-                                     conf.GRID_SQUARE_SIZE,
-                                     conf.GRID_SQUARE_SIZE)
-                conf.screen.blit(conf.floor_img, floor_rect)
-                strawb_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
-                    ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1], conf.GRID_SQUARE_SIZE,
-                    conf.GRID_SQUARE_SIZE)
-                conf.screen.blit(conf.strawb_img, strawb_rect)
+    def draw_player(self, player):
+        self.player = player
+        rect = pg.Rect((self.center_x - 1)*conf.GRID_SQUARE_SIZE, (self.center_y-1)*conf.GRID_SQUARE_SIZE,
+                       conf.GRID_SQUARE_SIZE, conf.GRID_SQUARE_SIZE)
+        conf.screen.blit(conf.player_img, rect)
 
-    # This draws the player
-    player_rect = pg.Rect((conf.CENTER_X - 1) * conf.GRID_SQUARE_SIZE, (conf.CENTER_Y - 1) * conf.GRID_SQUARE_SIZE,
-                                  conf.GRID_SQUARE_SIZE,
-                                  conf.GRID_SQUARE_SIZE)
-    conf.screen.blit(conf.player_img, player_rect)
-    # This draws the score count
-    score_text = conf.FONT.render("Score - {0}".format(conf.SCORE), 1, (255, 255, 255))
-    conf.screen.blit(score_text, (10, 5))
+    def draw_entities(self, entities):
+        for entity in entities:
+            relative_x_pixel = (((entity.x - self.player.x) + self.center_x) - 1) * conf.GRID_SQUARE_SIZE
+            relative_y_pixel = (((entity.y - self.player.y) + self.center_y) - 1) * conf.GRID_SQUARE_SIZE
+            print(relative_x_pixel, relative_y_pixel)
+            rect = pg.Rect(relative_x_pixel + self.player.offset[0], relative_y_pixel + self.player.offset[1],
+                           conf.GRID_SQUARE_SIZE, conf.GRID_SQUARE_SIZE)
+            conf.screen.blit(entity.image, rect)
+
+    def draw_overlay(self):
+        score_text = conf.FONT.render("Score - {0}".format(conf.SCORE), 1, (255, 255, 255))
+        conf.screen.blit(score_text, (10, 5))
+
+
+
+
+# def draw_camera_view(player):
+#     """
+#     This function will draw the current camera view of the game.
+#     It draws the relevant tiles only with a small overlap around the game window.
+#     It shifts the map by the current offset of the player.
+#     The player is always drawn in the center of the map.
+#     """
+#     current_grid_pos = (player.x, player.y)
+#     current_moving_offset = player.offset
+#
+#     # starting drawing points are off screen to allow for whichever direction the player is moving
+#     starting_x, starting_y = current_grid_pos[0] - conf.CENTER_X, current_grid_pos[1] - conf.CENTER_Y
+#     # This blanks the screen
+#     conf.screen.fill((0, 0, 0))
+#     # This loop draws the map around the player. It loops through with a 1 tile border.
+#     for y, row in enumerate(conf.COMPLETE_GRID[starting_y: starting_y+conf.CAMERA_HEIGHT+2]):
+#         for x, tile in enumerate(row[starting_x: starting_x+conf.CAMERA_WIDTH+2]):
+#             if tile == '#':
+#                 wall_rect = pg.Rect(((x-1) * conf.GRID_SQUARE_SIZE)+current_moving_offset[0], ((y-1) * conf.GRID_SQUARE_SIZE)+current_moving_offset[1], conf.GRID_SQUARE_SIZE,
+#                                       conf.GRID_SQUARE_SIZE)
+#                 conf.screen.blit(conf.wall_img, wall_rect)
+#             elif tile == ' ':
+#                 floor_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
+#                         ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1], conf.GRID_SQUARE_SIZE,
+#                         conf.GRID_SQUARE_SIZE)
+#                 conf.screen.blit(conf.floor_img, floor_rect)
+#             elif tile == 'T':
+#                 floor_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
+#                                      ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1],
+#                                      conf.GRID_SQUARE_SIZE,
+#                                      conf.GRID_SQUARE_SIZE)
+#                 conf.screen.blit(conf.floor_img, floor_rect)
+#                 strawb_rect = pg.Rect(((x - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[0],
+#                     ((y - 1) * conf.GRID_SQUARE_SIZE) + current_moving_offset[1], conf.GRID_SQUARE_SIZE,
+#                     conf.GRID_SQUARE_SIZE)
+#                 conf.screen.blit(conf.strawb_img, strawb_rect)
+#
+#     # This draws the player
+#     player_rect = pg.Rect((conf.CENTER_X - 1) * conf.GRID_SQUARE_SIZE, (conf.CENTER_Y - 1) * conf.GRID_SQUARE_SIZE,
+#                                   conf.GRID_SQUARE_SIZE,
+#                                   conf.GRID_SQUARE_SIZE)
+#     conf.screen.blit(conf.player_img, player_rect)
+#     # This draws the score count
+#     score_text = conf.FONT.render("Score - {0}".format(conf.SCORE), 1, (255, 255, 255))
+#     conf.screen.blit(score_text, (10, 5))
--- grid-based-game.py
+++ grid-based-game.py
@@ -6,37 +6,37 @@ import draw_screen as draw_to_screen
 
 
 def is_tile_walkable(x_test, y_test):
-    return True if conf.COMPLETE_GRID[y_test][x_test] == ' ' or conf.COMPLETE_GRID[y_test][x_test] == 'T' else False
+    return True if conf.COMPLETE_GRID[y_test][x_test] == ' ' else False
 
 
-def is_tile_this(x_test, y_test, test_tile):
-    return True if conf.COMPLETE_GRID[y_test][x_test] == test_tile else False
+# def is_tile_this(x_test, y_test, test_tile):
+#     return True if conf.COMPLETE_GRID[y_test][x_test] == test_tile else False
+#
 
-
-# TODO: map should never be changed. Tiles should be an instance of a Tile class. (Create this)
-def change_tile(x_pos, y_pos, new_tile):
-    lst_row = list(conf.COMPLETE_GRID[y_pos])
-    lst_row[x_pos] = new_tile
-    conf.COMPLETE_GRID[y_pos] = ''.join(lst_row)
-
-
-def gen_target():
-    target_x, target_y = random.randint(0, conf.GRID_WIDTH - 1), random.randint(0, conf.GRID_HEIGHT - 1)
-
-    while not is_tile_walkable(target_x, target_y) \
-            and not is_tile_this(target_x, target_y, 'T') \
-            and (target_x, target_y) != (player.x, player.y):
-        target_x, target_y = random.randint(0, conf.GRID_WIDTH - 1), random.randint(0, conf.GRID_HEIGHT - 1)
-    change_tile(target_x, target_y, 'T')
-    print("Target created at ({}, {})".format(target_x, target_y))
-
-
-def change_targets(old_grid_pos):
-    # if old pos was T then it must be emptied, score updated and new T generated
-    if is_tile_this(old_grid_pos[0], old_grid_pos[1], 'T'):
-        change_tile(old_grid_pos[0], old_grid_pos[1], ' ')
-        conf.SCORE += 1
-        gen_target()
+# # TODO: map should never be changed. Tiles should be an instance of a Tile class. (Create this)
+# def change_tile(x_pos, y_pos, new_tile):
+#     lst_row = list(conf.COMPLETE_GRID[y_pos])
+#     lst_row[x_pos] = new_tile
+#     conf.COMPLETE_GRID[y_pos] = ''.join(lst_row)
+#
+#
+# def gen_target():
+#     target_x, target_y = random.randint(0, conf.GRID_WIDTH - 1), random.randint(0, conf.GRID_HEIGHT - 1)
+#
+#     while not is_tile_walkable(target_x, target_y) \
+#             and not is_tile_this(target_x, target_y, 'T') \
+#             and (target_x, target_y) != (player.x, player.y):
+#         target_x, target_y = random.randint(0, conf.GRID_WIDTH - 1), random.randint(0, conf.GRID_HEIGHT - 1)
+#     change_tile(target_x, target_y, 'T')
+#     print("Target created at ({}, {})".format(target_x, target_y))
+#
+#
+# def change_targets(old_grid_pos):
+#     # if old pos was T then it must be emptied, score updated and new T generated
+#     if is_tile_this(old_grid_pos[0], old_grid_pos[1], 'T'):
+#         change_tile(old_grid_pos[0], old_grid_pos[1], ' ')
+#         conf.SCORE += 1
+#         gen_target()
 
 
 def update_grid_pos(old_grid_pos):
@@ -61,11 +61,13 @@ def update_grid_pos(old_grid_pos):
     new_grid_y = min(conf.GRID_HEIGHT - 1, max(new_grid_y, 0))
     new_grid_pos = (new_grid_x, new_grid_y)
     player.reset_offset(new_grid_pos, old_grid_pos)
-    change_targets(old_grid_pos)
+    # change_targets(old_grid_pos)
     return new_grid_pos
 
 
 #gen_target()
+camera = draw_to_screen.Camera(11, 13)
+enemies = server.get_enemies()
 while not conf.done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -78,8 +80,11 @@ while not conf.done:
     player.reduce_offset()
     server.set_player(player)
     # DRAW
-    draw_to_screen.draw_camera_view(player)
-
+    #draw_to_screen.draw_camera_view(player)
+    camera.draw_camera_map(player.x, player.y, player.offset)
+    camera.draw_player(player)
+    camera.draw_entities(enemies)
+    camera.draw_overlay()
     # UPDATE
     pg.display.flip()
     conf.clock.tick(conf.FPS)
